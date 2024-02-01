b60a1ed051d208af538121dbc6f9d117c28a5c29
Rui Duram <ruiduran.jr@gmail.com>
2024-02-01 19:38:58 +0100

    unfinished game

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 class Player {
   constructor() {
-    this.width = 10;
+    this.width = 5;
     this.height = 10;
     this.positionX = 50;
     this.positionY = 0;
     this.domElm = null;
+    this.score = 0;
 
     this.createDomElement();
   }
@@ -44,17 +45,22 @@ class Player {
       this.domElm.style.bottom = this.positionY + "vh";
     }
   }
+  increaseScore() {
+    this.score++;
+    let scorePoints = document.querySelector(`#playerPoints`);
+    scorePoints.textContent = `Score:${this.score}`;
+  }
 }
+
 class Collectible {
   constructor() {
-    this.width = 5;
+    this.width = 3;
     this.height = 5;
     this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
     this.positionY = 100;
     this.domElm = null;
     this.createDomElement();
   }
-
   createDomElement() {
     this.domElm = document.createElement("div");
     this.domElm.setAttribute("class", "collectible");
@@ -68,11 +74,19 @@ class Collectible {
   }
 
   moveDown() {
-    this.positionY--;
-    this.domElm.style.bottom = this.positionY + "vh";
+    if (this.positionY > 0) {
+      this.positionY--;
+      this.domElm.style.bottom = this.positionY + "vh";
+    } else {
+      gameOver();
+      removeItem(this);
+    }
   }
-  // Implement left spawning
 }
+function gameOver() {
+  location.href = "gameover.html";
+}
+
 const player = new Player();
 const Collectibles = [];
 
@@ -82,20 +96,79 @@ setInterval(() => {
 }, 3000);
 
 setInterval(() => {
-  Collectibles.forEach((collectibleInstance) => {
-    collectibleInstance.moveDown();
+  Collectibles.forEach((collectible) => {
+    collectible.moveDown();
+
+    if (
+      player.positionX < collectible.positionX + collectible.width &&
+      player.positionX + player.width > collectible.positionX &&
+      player.positionY < collectible.positionY + collectible.height &&
+      player.positionY + player.height > collectible.positionY
+    ) {
+      removeItem(collectible);
+      player.increaseScore();
+    }
   });
-}, 50);
-//                                                              Implement score system
+}, 30);
+function removeItem(item) {
+  const itemIndex = Collectibles.indexOf(item);
+  if (itemIndex !== -1) {
+    Collectibles.splice(itemIndex, 1);
+  }
+
+  const boardElm = document.getElementById("playingfield");
+  boardElm.removeChild(item.domElm);
+}
+// Implement score system
+const keysPressed = {};
 
 document.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowLeft") {
-    player.moveLeft();
-  } else if (e.code === "ArrowRight") {
-    player.moveRight();
-  } else if (e.code === "ArrowUp") {
-    player.moveUp();
-  } else if (e.code === "ArrowDown") {
-    player.moveDown();
+  if (
+    e.code === "ArrowLeft" ||
+    e.code === "ArrowRight" ||
+    e.code === "ArrowUp" ||
+    e.code === "ArrowDown"
+  ) {
+    keysPressed[e.code] = true;
   }
 });
+
+document.addEventListener("keyup", (e) => {
+  if (
+    e.code === "ArrowLeft" ||
+    e.code === "ArrowRight" ||
+    e.code === "ArrowUp" ||
+    e.code === "ArrowDown"
+  ) {
+    keysPressed[e.code] = false;
+  }
+});
+
+setInterval(() => {
+  const diagonalSpeed = 1.5;
+
+  if (keysPressed["ArrowLeft"] && keysPressed["ArrowUp"]) {
+    player.moveLeft();
+    player.moveUp();
+  } else if (keysPressed["ArrowLeft"] && keysPressed["ArrowDown"]) {
+    player.moveLeft();
+    player.moveDown();
+  } else if (keysPressed["ArrowRight"] && keysPressed["ArrowUp"]) {
+    player.moveRight();
+    player.moveUp();
+  } else if (keysPressed["ArrowRight"] && keysPressed["ArrowDown"]) {
+    player.moveRight();
+    player.moveDown();
+  } else {
+    if (keysPressed["ArrowLeft"]) {
+      player.moveLeft();
+    } else if (keysPressed["ArrowRight"]) {
+      player.moveRight();
+    }
+    if (keysPressed["ArrowUp"]) {
+      player.moveUp();
+    } else if (keysPressed["ArrowDown"]) {
+      player.moveDown();
+    }
+  }
+}, 30);
